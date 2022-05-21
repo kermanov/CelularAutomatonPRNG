5a76f09b49b97cd8db8f488b17928c7829514f03
Maksym Dovhan <kerman901@gmail.com>
2022-05-21 16:28:46 +0300

    Add CellularAutomatonRandom45.

---
+++ src/CelularAutomatonPRNG/CelularAutomatonPRNG/CellularAutomatonRandom45.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CelularAutomatonPRNG
+{
+    class CellularAutomatonRandom45 : CellularAutomatonRandom
+    {
+        private const byte _rule = 45;
+
+        public CellularAutomatonRandom45(int seed) : base(_rule, seed)
+        {
+        }
+
+        public CellularAutomatonRandom45() : base(_rule)
+        {
+        }
+    }
+}
